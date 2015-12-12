9637cb630f437094e3587265597252b10afbb9fc
Rusty Bentley <mycoding.alert@gmail.com>
2015-12-12 17:21:44 -0600

    initial commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to cartagena
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/cartagena/core_test.clj
@@ -0,0 +1,7 @@
+(ns cartagena.core-test
+  (:require [clojure.test :refer :all]
+            [cartagena.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
