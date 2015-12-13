60a5691136a091c4cc18a33cb7b74a57ccf189ee
Rusty Bentley <mycoding.alert@gmail.com>
2015-12-13 17:28:18 -0600

    initialize-board works

--- src/cartagena/core.clj
+++ src/cartagena/core.clj
@@ -1,6 +1,30 @@
 (ns cartagena.core
   (:gen-class))
 
+(def card1 [:bottle :gun :hat :skull :knife :key])
+(def card2 [:knife :bottle :key :gun :hat :skull])
+(def card3 [:hat :key :gun :bottle :skull :knife])
+(def card4 [:key :bottle :skull :knife :hat :gun])
+(def card5 [:gun :key :knife :hat :skull :bottle])
+(def card6 [:hat :knife :key :bottle :gun :skull])
+(def card1r (vec (reverse card1)))
+(def card2r (vec (reverse card2)))
+(def card3r (vec (reverse card3)))
+(def card4r (vec (reverse card4)))
+(def card5r (vec (reverse card5)))
+(def card6r (vec (reverse card6)))
+(def all-cards [card1 card2 card3 card4 card5 card6
+                card1r card2r card3r card4r card5r card6r])
+
+(defn initialize-board
+  "Returns a vector populated with icons from the 6 of the board pieces concatenated."
+  []
+  (->> all-cards
+       shuffle
+       (take 6)
+       flatten
+       vec))
+
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
--- test/cartagena/core_test.clj
+++ test/cartagena/core_test.clj
@@ -2,6 +2,8 @@
   (:require [clojure.test :refer :all]
             [cartagena.core :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(deftest initialize-board-test
+  (let [board (initialize-board)]
+    (is (= 36 (count board)))
+    (doseq [icon [:bottle :gun :hat :key :knife :skull]]
+      (is (= 6 (count (filter #(= icon %) board)))))))
