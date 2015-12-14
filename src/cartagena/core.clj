e7e0acd072198dd788e5fc644c583fb4bc8be7df
Rusty Bentley <mycoding.alert@gmail.com>
2015-12-14 15:24:41 -0600

    renamed "card" to "board-piece"

--- src/cartagena/core.clj
+++ src/cartagena/core.clj
@@ -1,25 +1,25 @@
 (ns cartagena.core
   (:gen-class))
 
-(def card1 [:bottle :gun :hat :skull :knife :key])
-(def card2 [:knife :bottle :key :gun :hat :skull])
-(def card3 [:hat :key :gun :bottle :skull :knife])
-(def card4 [:key :bottle :skull :knife :hat :gun])
-(def card5 [:gun :key :knife :hat :skull :bottle])
-(def card6 [:hat :knife :key :bottle :gun :skull])
-(def card1r (vec (reverse card1)))
-(def card2r (vec (reverse card2)))
-(def card3r (vec (reverse card3)))
-(def card4r (vec (reverse card4)))
-(def card5r (vec (reverse card5)))
-(def card6r (vec (reverse card6)))
-(def all-cards [card1 card2 card3 card4 card5 card6
-                card1r card2r card3r card4r card5r card6r])
+(def board-piece1 [:bottle :gun :hat :skull :knife :key])
+(def board-piece2 [:knife :bottle :key :gun :hat :skull])
+(def board-piece3 [:hat :key :gun :bottle :skull :knife])
+(def board-piece4 [:key :bottle :skull :knife :hat :gun])
+(def board-piece5 [:gun :key :knife :hat :skull :bottle])
+(def board-piece6 [:hat :knife :key :bottle :gun :skull])
+(def board-piece1r (vec (reverse board-piece1)))
+(def board-piece2r (vec (reverse board-piece2)))
+(def board-piece3r (vec (reverse board-piece3)))
+(def board-piece4r (vec (reverse board-piece4)))
+(def board-piece5r (vec (reverse board-piece5)))
+(def board-piece6r (vec (reverse board-piece6)))
+(def all-board-pieces [board-piece1 board-piece2 board-piece3 board-piece4 board-piece5 board-piece6
+                board-piece1r board-piece2r board-piece3r board-piece4r board-piece5r board-piece6r])
 
 (defn initialize-board
   "Returns a vector populated with icons from the 6 of the board pieces concatenated."
   []
-  (->> all-cards
+  (->> all-board-pieces
        shuffle
        (take 6)
        flatten
