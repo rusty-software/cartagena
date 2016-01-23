fa867a48559c3ac67c44004bbba9bcf3f4aa66c4
Rusty Bentley <mycoding.alert@gmail.com>
2016-01-23 09:52:21 -0600

    adding images, but can't get svg image to work

--- cartagena-web/src/cartagena_web/core.cljs
+++ cartagena-web/src/cartagena_web/core.cljs
@@ -9,6 +9,9 @@
 
 (defonce app-state (atom nil))
 
+(defn jail-img []
+  [:img {:src "img/jail.png" :width 30 :height 30 :left 0 :top 0}])
+
 (defn jail []
   [:rect {:x 0 :y 0 :width 50 :height 90 :stroke "black" :fill "brown"}])
 
@@ -22,7 +25,8 @@
        :height 600}
       [:rect {:x 0 :y 0 :width 800 :height 600 :stroke "black" :stroke-width "1" :fill "tan"}]
       [:rect {:x 0 :y 0 :width 500 :height 400 :stroke "black" :stroke-width "0.5" :fill "burlywood"}]
-      (jail)]]]])
+      (jail)
+      (jail-img)]]]])
 
 (defn ^:export main []
   (when-let [app (. js/document (getElementById "app"))]
